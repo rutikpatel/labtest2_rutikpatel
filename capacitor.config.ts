b92af7ed785c96073e0285d9e39481702c5711d1
Rutik Patel <rutikpatel3660@gmail.com>
2022-03-30 14:56:39 -0400

    adds capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'rutik.patel.test',
+  appName: 'labtest2_rutikpatel',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
